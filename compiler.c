ebe6eb8254011e3d386cc0f1ca95079fa92683ad
David Ensikat <densikat@gmail.com>
2023-07-11 16:11:29 +1000

    Chapter 21 and 22 done (hash table and global variables)

--- compiler.c
+++ compiler.c
@@ -35,7 +35,7 @@ typedef enum {
     PREC_PRIMARY
 } Precedence;
 
-typedef void (*ParseFn)();
+typedef void (*ParseFn)(bool canAssign);
 
 typedef struct {
     ParseFn prefix;
@@ -46,6 +46,10 @@ typedef struct {
 
 // Forward declarations
 ParseRule rules[];
+static void consume(TokenType type, const char* message);
+static bool match(TokenType type);
+static void statement();
+static void declaration();
 static ParseRule* getRule(TokenType type);
 
 // Globals
@@ -131,13 +135,32 @@ static void parsePrecedence(Precedence precedence) {
         error("Expected expression.");
         return;
     }
-    prefixRule();
+	bool canAssign = precedence <= PREC_ASSIGNMENT;
+	prefixRule(canAssign);
 
     while (precedence <= getRule(parser.current.type)->precedence) {
         advance();
         ParseFn infixRule = getRule(parser.previous.type)->infix;
-        infixRule();
+        infixRule(canAssign);
     }
+
+	if (canAssign && match(TOKEN_EQUAL)) {
+	  error("Invalid assignment target.");
+	}
+}
+
+static uint8_t identifierConstant(Token *name) {
+  return makeConstant(OBJ_VAL(copyString(name->start,
+										 name->length)));
+}
+
+static uint8_t parseVariable(const char* errorMessage) {
+  consume(TOKEN_IDENTIFIER, errorMessage);
+  return identifierConstant(&parser.previous);
+}
+
+static void defineVariable(uint8_t global) {
+  emitBytes(OP_DEFINE_GLOBAL, global);
 }
 
 static ParseRule* getRule(TokenType type) {
@@ -146,6 +169,73 @@ static ParseRule* getRule(TokenType type) {
 
 static void expression() {
     parsePrecedence(PREC_ASSIGNMENT);
+}
+
+static void varDeclaration() {
+  uint8_t global = parseVariable("Expect variable name.");
+
+  if (match(TOKEN_EQUAL)) {
+	expression();
+  } else {
+	emitByte(OP_NIL);
+  }
+  consume(TOKEN_SEMICOLON,
+		  "Expect ';' after variable declaration.");
+  defineVariable(global);
+}
+
+static void expressionStatement() {
+  expression();
+  consume(TOKEN_SEMICOLON, "Expect ';' after expression.");
+  emitByte(OP_POP);
+}
+
+static void printStatement() {
+  expression();
+  consume(TOKEN_SEMICOLON, "Expect ';' after value.");
+  emitByte(OP_PRINT);
+}
+
+static void synchronize() {
+  parser.panicMode = false;
+
+  while (parser.current.type != TOKEN_EOF) {
+	if (parser.previous.type == TOKEN_SEMICOLON) return;
+
+	switch (parser.current.type) {
+	  case TOKEN_CLASS:
+	  case TOKEN_FUN:
+	  case TOKEN_VAR:
+	  case TOKEN_FOR:
+	  case TOKEN_IF:
+	  case TOKEN_WHILE:
+	  case TOKEN_PRINT:
+	  case TOKEN_RETURN:
+		return;
+	  default:
+		; // Do nothing
+	}
+
+	advance();
+  }
+}
+
+static void declaration() {
+  if (match(TOKEN_VAR)) {
+	varDeclaration();
+  } else {
+	statement();
+  }
+
+  if (parser.panicMode) synchronize();
+}
+
+static void statement() {
+  if (match(TOKEN_PRINT)) {
+	printStatement();
+  } else {
+	expressionStatement();
+  }
 }
 
 static void consume(TokenType type, const char* message) {
@@ -157,11 +247,21 @@ static void consume(TokenType type, const char* message) {
     errorAtCurrent(message);
 }
 
+static bool check(TokenType type) {
+  return parser.current.type == type;
+}
+
+static bool match(TokenType type) {
+  if (!check(type)) return false;
+  advance();
+  return true;
+}
+
 static void emitConstant(Value value) {
     emitBytes(OP_CONSTANT, makeConstant(value));
 }
 
-static void binary() {
+static void binary(bool canAssign) {
     TokenType operatorType = parser.previous.type;
     ParseRule* rule = getRule(operatorType);
     parsePrecedence((Precedence)(rule->precedence + 1));
@@ -189,7 +289,7 @@ static void binary() {
     }
 }
 
-static void literal() {
+static void literal(bool canAssign) {
   switch (parser.previous.type) {
 	case TOKEN_FALSE: emitByte(OP_FALSE); break;
 	case TOKEN_NIL: emitByte(OP_NIL); break;
@@ -198,17 +298,32 @@ static void literal() {
   }
 }
 
-static void number() {
+static void number(bool canAssign) {
     double value = strtod(parser.previous.start, NULL);
     emitConstant(NUMBER_VAL(value));
 }
 
-static void string() {
+static void string(bool canAssign) {
   emitConstant(OBJ_VAL(copyString(parser.previous.start + 1,
 								  parser.previous.length - 2)));
 }
 
-static void unary() {
+static void namedVariable(Token name, bool canAssign) {
+  uint8_t arg = identifierConstant(&name);
+
+  if (canAssign && match(TOKEN_EQUAL)) {
+	expression();
+	emitBytes(OP_SET_GLOBAL, arg);
+  } else {
+	emitBytes(OP_GET_GLOBAL, arg);
+  }
+}
+
+static void variable(bool canAssign) {
+  namedVariable(parser.previous, canAssign);
+}
+
+static void unary(bool canAssign) {
     TokenType operatorType = parser.previous.type;
 
     // Compile the operand
@@ -224,7 +339,7 @@ static void unary() {
     }
 }
 
-static void grouping() {
+static void grouping(bool canAssign) {
     expression();
     consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
 }
@@ -249,7 +364,7 @@ ParseRule rules[] = {
         [TOKEN_GREATER_EQUAL] = {NULL,     binary,   PREC_COMPARISON},
         [TOKEN_LESS]          = {NULL,     binary,   PREC_COMPARISON},
         [TOKEN_LESS_EQUAL]    = {NULL,     binary,   PREC_COMPARISON},
-        [TOKEN_IDENTIFIER]    = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_IDENTIFIER]    = {variable,     NULL,   PREC_NONE},
         [TOKEN_STRING]        = {string,     NULL,   PREC_NONE},
         [TOKEN_NUMBER]        = {number,   NULL,   PREC_NONE},
         [TOKEN_AND]           = {NULL,     NULL,   PREC_NONE},
@@ -280,8 +395,11 @@ bool compile(const char* source, Chunk* chunk) {
   parser.panicMode = false;
 
   advance();
-  expression();
-  consume(TOKEN_EOF, "Expected end of expression.");
+
+  while (!match(TOKEN_EOF)) {
+	declaration();
+  }
+
   endCompiler();
   return !parser.hadError;
 }
--- object.c
+++ object.c
@@ -61,12 +61,12 @@ void printObject(Value value) {
 }
 
 ObjString *takeString(char *chars, int length) {
+  uint32_t hash = hashString(chars, length);
   ObjString* interned = tableFindString(&vm.strings, chars, length, hash);
 
   if (interned != NULL) {
 	FREE_ARRAY(char, chars, length + 1);
 	return interned;
   }
-  uint32_t hash = hashString(chars, length);
   return allocateString(chars, length, hash);
 }
